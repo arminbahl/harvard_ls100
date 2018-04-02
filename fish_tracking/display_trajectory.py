bd624902b4e4c74ea6ff0a6e940184109a1c2b88
arminbahl <arbahl@gmail.com>
2018-04-02 01:13:30 -0400

    setting up the get repository for the LS100 course

--- fish_tracking/display_trajectory.py
+++ fish_tracking/display_trajectory.py
@@ -1,16 +1,22 @@
 import numpy as np
 import os
 import pylab as pl
-root_path = r"D:\Zebrafish"
+import cv2
+import sys
+
+
+root_path = r"/Users/arminbahl/Desktop/ls100"
 
 # a list of all the fish where the background should be calculated
-fish_names = ["fc2_save_2018-02-26-150603.avi"]
+fish_names = ["fish8_0316_20866_7dpf.avi"]
 
 for fish_name in fish_names:
     path = os.path.join(root_path, fish_name)[:-4] + "_extracted_x_y_ang.npy"
     fish_data = np.load(path)
 
     print(fish_data.shape)
+    pl.xlim(0, 700)
+    pl.ylim(0, 700)
 
     pl.plot(fish_data[:,1], fish_data[:,2])
     pl.show()
--- fish_tracking/extract_fish_position_orientation.py
+++ fish_tracking/extract_fish_position_orientation.py
@@ -9,12 +9,9 @@ import matplotlib
 matplotlib.use("qt5agg")
 import pylab as pl
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter
 import os
 import cv2
 import sys
-
-
 
 def get_fish_position_and_angle(frame, background, threshold, filter_width, display):
 
@@ -23,18 +20,16 @@ def get_fish_position_and_angle(frame, background, threshold, filter_width, disp
     # make the fish the brightest area in the image
     # because movie and background are likely of type unsigned int, for the sake
     # of the substraction, make them signed integers
-    background_substracted_image = np.abs(background.astype(np.int) -
-                                          frame.astype(np.int))
+    background_substracted_image = np.abs(background.astype(np.int) - frame.astype(np.int)).astype(np.uint8)
 
     # threshold, play around with the tresholding paramater for optimal results
-    fish_image_thresholded = background_substracted_image.copy()
-    fish_image_thresholded[fish_image_thresholded < threshold] = 0
-    fish_image_thresholded[fish_image_thresholded >= threshold] = 255
+    #print(background_substracted_image.shape, background_substracted_image.dtype)
+    ret, fish_image_thresholded = cv2.threshold(background_substracted_image, threshold, 255, cv2.THRESH_BINARY)
 
     # apply a weak gaussian blur to the thresholded image to get rid of noisy pixels
     # play around with the standard deviation for optimal results, normally, if little noise
     # use small values
-    fish_image_blurred = gaussian_filter(fish_image_thresholded, (filter_width, filter_width))
+    fish_image_blurred = cv2.GaussianBlur(fish_image_thresholded, (filter_width, filter_width), 0)
 
     # find the position of the maximum with
     x, y = np.unravel_index(np.argmax(fish_image_blurred), fish_image_blurred.shape)
@@ -62,79 +57,69 @@ def get_fish_position_and_angle(frame, background, threshold, filter_width, disp
         y_down = background_substracted_image.shape[1] - 1
         y_up = y_down - 100
 
-    # copy that region from the thresholded image
-    fish_roi_cutout = fish_image_thresholded[x_left:x_right, y_up:y_down].copy()
+    # copy that region from the thresholded image and resize a little
+    fish_roi_cutout = cv2.resize(fish_image_thresholded[x_left:x_right, y_up:y_down], (200, 200))
 
-    # translate the image pixels into a vector of x,y
-    coordinates_original_cutout = np.array(np.where(fish_roi_cutout == 255))
+    im2, contours, hierarchy = cv2.findContours(fish_roi_cutout, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    # find the biggest contour
+    contour_areas = [cv2.contourArea(contour) for contour in contours]
+    cnt = contours[np.argmax(contour_areas)]
+
+    # determine a convex hull around that contour
+    hull = cv2.convexHull(cnt)
 
     # if we found not enough fish points
-    if coordinates_original_cutout.shape[1] < 10:
+    if hull.shape[0] < 3:
         print("No fish found. Please check thresholding parameters, etc.")
         return np.nan, np.nan, np.nan
 
-    # determine the center of mass of the cutout
-    x_cutout = coordinates_original_cutout[0].mean()
-    y_cutout = coordinates_original_cutout[1].mean()
-
     # move the fish coordinates to the center
-    coordinates_moved_cutout = coordinates_original_cutout.copy()
-    coordinates_moved_cutout[0] = coordinates_moved_cutout[0] - x_cutout
-    coordinates_moved_cutout[1] = coordinates_moved_cutout[1] - y_cutout
+    hull_moved = hull.copy()
+    hull_moved[:, :, 0] = hull_moved[:, :, 0] - hull[:, 0, 0].mean()
+    hull_moved[:, :, 1] = hull_moved[:, :, 1] - hull[:, 0, 1].mean()
 
-    # the eigenvectors of the covariance matrix determine
-    # the axes of a coordinate system along the largest/smallest variance
-    cov = np.cov(coordinates_moved_cutout)
-    evals, evecs = np.linalg.eig(cov)
-    sort_indices = np.argsort(evals)[::-1]
-    x_v1, y_v1 = evecs[:, sort_indices[0]]  # Eigenvector with largest eigenvalue
-    x_v2, y_v2 = evecs[:, sort_indices[1]]
+    moments = cv2.moments(hull_moved)
+    mu20 = moments["mu20"] / moments["m00"]
+    mu02 = moments["mu02"] / moments["m00"]
+    mu11 = moments["mu11"] / moments["m00"]
 
-    # what is the angle of largest axes relative to our original axis? - this is the fish orienteation
-    fish_orientation = np.tanh(x_v1 / y_v1)
+    fish_orientation = 0.5*np.arctan2(2 * mu11, mu20 - mu02)
 
-    # create a rotation matrix with that angle
-    rotation_mat = np.matrix([[np.cos(fish_orientation), -np.sin(fish_orientation)],
-                              [np.sin(fish_orientation), np.cos(fish_orientation)]])
+    M = cv2.getRotationMatrix2D((200 / 2, 200 / 2), fish_orientation * 180/np.pi, 1)
 
+    dummy_image_original = np.zeros((200, 200), dtype=np.uint8)
 
-    # rotate the mean-centered fish points according to that angle
-    coordinates_rotated_cutout = np.array(rotation_mat * coordinates_moved_cutout)
+    cv2.drawContours(dummy_image_original, [hull_moved+100], 0, 255, cv2.FILLED, 8)
+    dummy_image_rotated = cv2.warpAffine(dummy_image_original, M, (200, 200))
 
-    # if more dots are negative than positive in the rotated fish, the head looks downwards.
-    # In that case add 180 degrees to the fish orientation
-    ind_positive = coordinates_rotated_cutout[1] > 0
-    ind_negative = coordinates_rotated_cutout[1] < 0
+    fish_width = np.sum(dummy_image_rotated.copy().astype(np.int), axis=0)
 
-    if ind_negative.sum() > ind_positive.sum():
+    if np.argmax(fish_width) < 100:
         fish_orientation += np.pi
 
-    # make it degrees
-    fish_orientation = fish_orientation * 180 / np.pi
-
-    # put it in the right range
-    fish_orientation = (fish_orientation + 360) % 360
-
-
-    # we are done. ;-)
-
-    # if desired, display all the processing stages
     if display:
+        img = np.zeros((200, 200)).astype(np.uint8)
+        cv2.drawContours(img, [hull_moved + 100], 0, 128)
+
+        dy = int(np.cos(fish_orientation) * 20)
+        dx = int(np.sin(fish_orientation) * 20)
+        cv2.line(img, (100, 100), (100+dy, 100+dx), 255, thickness=2)
+        cv2.imshow("test2", img)
+        cv2.waitKey(1)
 
         stiched_images = np.concatenate((image, fish_image_thresholded, fish_image_blurred), axis=1)
         image_for_display = cv2.cvtColor(stiched_images.copy().astype(np.uint8), cv2.COLOR_GRAY2BGR)
         # draw into the frame for displaying
-        dy = int(np.cos(fish_orientation * np.pi / 180) * 20)
-        dx = int(np.sin(fish_orientation * np.pi / 180) * 20)
+        dy = int(np.cos(fish_orientation) * 20)
+        dx = int(np.sin(fish_orientation) * 20)
 
-        print(fish_image_thresholded.dtype)
         cv2.line(image_for_display, (y, x), (y + dy, x + dx), (0, 255, 0), thickness=2)
         cv2.circle(image_for_display, (y + dy, x + dx), 3, (0, 0, 255), thickness=-1)
 
         image_for_display = cv2.resize(image_for_display, None, fx=0.8, fy=0.8)
         cv2.imshow("fish image", image_for_display)
 
-        cv2.imshow("fish roi", fish_roi_cutout.astype(np.uint8))
         if cv2.waitKey(1) == 27:
             sys.exit()
 
@@ -163,11 +148,11 @@ def get_fish_position_and_angle(frame, background, threshold, filter_width, disp
         pl.title("Stage 4: Cut out region of interest")
         pl.imshow(fish_roi_cutout, cmap='gray')
         pl.show()
-
+        
         ####
         # original fish coordinates and center-moved corrdinates
         pl.title("Stage 5: Extract coordinates and move to center of mass")
-        pl.plot(coordinates_original_cutout[0], coordinates_original_cutout[1], '.')
+        #pl.plot(coordinates_original_cutout[0], coordinates_original_cutout[1], '.')
         pl.plot(coordinates_moved_cutout[0], coordinates_moved_cutout[1], '.')
 
         # the principle axes of the new coordinate system
@@ -179,7 +164,7 @@ def get_fish_position_and_angle(frame, background, threshold, filter_width, disp
                 [y_v2 * -scale, y_v2 * scale], color='blue')
 
         pl.show()
-
+        
         pl.title("Stage 6: Rotate and determine number of points below and above zero")
         pl.plot(coordinates_rotated_cutout[0], coordinates_rotated_cutout[1], '.')
         pl.plot(coordinates_rotated_cutout[0][ind_positive], coordinates_rotated_cutout[1][ind_positive], '+')  # color the positive ones
@@ -201,7 +186,7 @@ def get_fish_position_and_angle(frame, background, threshold, filter_width, disp
 
 
     # swap y, and x,
-    return int(y), int(x), 360-fish_orientation
+    return int(y), int(frame.shape[0]-x), -fish_orientation*180/np.pi
 
 #display an example fish
 """
@@ -226,10 +211,10 @@ print("Fish angle in image (in degrees): ", fish_angle * 180/np.pi)
 # Analyzing the fish list
 
 # this is the path where all the fish movies reside
-root_path = r"D:\Zebrafish"
+root_path = r"/Users/arminbahl/Desktop/ls100"
 
 # a list of all the fish where the background should be calculated
-fish_names = ["fc2_save_2018-02-26-150603.avi"]
+fish_names = ["fish8_0316_20866_7dpf.avi"]
 
 # loop through all those fish names, and calculate their backgroud images
 for fish_name in fish_names:
@@ -258,8 +243,8 @@ for fish_name in fish_names:
         image = frame[:, :, 0]
         x, y, fish_orientation = get_fish_position_and_angle(image,
                                                              background,
-                                                             threshold = 35,
-                                                             filter_width = 2,
+                                                             threshold=20,
+                                                             filter_width=5, # has to be odd
                                                              display=False)
 
         ts.append(frame_counter * dt)
