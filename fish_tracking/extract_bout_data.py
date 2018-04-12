5b43b5501c5274bbd97b9c5d93a487bcaec47f75
arminbahl <arbahl@gmail.com>
2018-04-12 13:37:39 -0400

    updated tracking, now using image moments for determining the fish orientation.

--- fish_tracking/extract_bout_data.py
+++ fish_tracking/extract_bout_data.py
@@ -59,55 +59,72 @@ bout_path_changes = accumulated_path[event_start_indices + int(0.5/dt)] - \
 
 interbout_intervals = np.diff(bout_start_times)
 
+center_distance = np.sqrt((x-352)**2 + (y-352)**2)
 
-# Plot and extract features
 
+##################
 pl.figure()
 pl.title("Fish trajectory")
 pl.plot(x, y)
 pl.plot(x[event_start_indices], y[event_start_indices], 'ro')
-pl.show()
 
+##################
 pl.figure()
 pl.title("Rolling variance")
 pl.plot(t, data_rolling_var)
 pl.plot(t[event_start_indices], data_rolling_var[event_start_indices], 'ro')
 
+##################
 pl.figure()
 pl.title("Accumulated path")
 pl.plot(t[1:], accumulated_path)
 pl.plot(t[event_start_indices], accumulated_path[event_start_indices], 'ro')
 
+##################
 pl.figure()
 pl.title("Accumulated angle")
 pl.plot(t, accummulated_ang)
 pl.plot(t[event_start_indices], accummulated_ang[event_start_indices], 'ro')
 
+##################
 a,b = np.histogram(bout_path_changes, bins=np.linspace(0, 100, 30), density=True)
 pl.figure()
 pl.title("Swim forward angle histogram densities")
 pl.plot(b[1:], a)
+print("Swim forward angle histogram bins: ", b[1:])
+print("Swim forward angle histogram densities: ", a)
 print("Median forward swims: ", np.nanmedian(np.abs(bout_path_changes)))
+print("\n\n")
 
-# plot the histogram of turning angles (density plot, could be averaged over multiple fish)
+##################
 a, b = np.histogram(bout_angle_changes, bins=np.linspace(-60, 60, 30), density=True)
-print("Turn angle histogram densities", a)
 pl.figure()
 pl.title("Turn angle histogram densities")
 pl.plot(b[1:], a)
+print("Turn angle histogram bins", b[1:])
+print("Turn angle histogram densities", a)
 print("Median absolute turn angle:", np.nanmedian(np.abs(bout_angle_changes)))
-
-### Median to center
-center_distance = np.sqrt((x-352)**2 + (y-352)**2)
-print("Median distance to the center: ", np.nanmedian(center_distance))
+print("\n\n")
 
 ##################
-print("Average interbout interval: ", np.nanmedian(interbout_intervals))
+a, b = np.histogram(center_distance, bins=np.linspace(0, 400, 30), density=True)
+pl.figure()
+pl.title("Center distance densities")
+pl.plot(b[1:], a)
+print("Center distance densities bins: ", b[1:])
+print("Center distance densities: ", a)
+print("Median distance to the center: ", np.nanmedian(center_distance))
+print("\n\n")
 
+##################
 a,b = np.histogram(interbout_intervals, bins=np.linspace(0, 3, 30), density=True)
 pl.figure()
 pl.title("Interbout interval histograms")
 pl.plot(b[1:], a)
+print("Interbout interval bins:", b[1:])
 print("Interbout interval densities: ", a)
+
+
+
 pl.show()
 
