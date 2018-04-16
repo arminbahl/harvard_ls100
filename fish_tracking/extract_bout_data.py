29fcb9c816efa981eada6fb75f5a0f48a57dff16
arminbahl <arbahl@gmail.com>
2018-04-16 15:58:15 -0400

    feature extraction for stats from single fish

--- fish_tracking/extract_bout_data.py
+++ fish_tracking/extract_bout_data.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use('qt5agg')
 import pylab as pl
 import pandas as pd
+from pandas import DataFrame
 
 
 fish_data = np.load("/Users/arminbahl/Desktop/fish15_0321_tlab_5dpf_extracted_x_y_ang.npy")
@@ -63,44 +64,70 @@ pl.plot(t, accummulated_ang)
 pl.plot(t[event_start_indices], accummulated_ang[event_start_indices], 'ro')
 
 ##################
-a,b = np.histogram(bout_path_changes, bins=np.linspace(0, 100, 30), density=True)
+swims_forward_densities, swims_forward_bins = np.histogram(bout_path_changes, bins=np.linspace(0, 100, 30), density=True)
 pl.figure()
 pl.title("Swim forward histogram densities")
-pl.plot(b[1:], a)
-print("Swims forward histogram bins: ", b[1:])
-print("Swims forward histogram densities: ", a)
+pl.plot(swims_forward_bins[1:], swims_forward_densities)
+print("Swims forward bins: ", swims_forward_bins[1:])
+print("Swims forward densities: ", swims_forward_densities)
 print("Median forward swims: ", np.nanmedian(np.abs(bout_path_changes)))
 print("\n\n")
 
 ##################
-a, b = np.histogram(bout_angle_changes, bins=np.linspace(-60, 60, 30), density=True)
+turn_angle_densties, turn_angle_bins = np.histogram(bout_angle_changes, bins=np.linspace(-60, 60, 30), density=True)
 pl.figure()
 pl.title("Turn angle histogram densities")
-pl.plot(b[1:], a)
-print("Turn angle histogram bins: ", b[1:])
-print("Turn angle histogram densities: ", a)
-print("Median absolute turn angle:", np.nanmedian(np.abs(bout_angle_changes)))
+pl.plot(turn_angle_bins[1:], turn_angle_densties)
+print("Turn angle histogram bins: ", turn_angle_bins[1:])
+print("Turn angle histogram densities: ", turn_angle_densties)
+print("Median absolute turn angle: ", np.nanmedian(np.abs(bout_angle_changes)))
 print("\n\n")
 
 ##################
-a, b = np.histogram(center_distance, bins=np.linspace(0, 400, 30), density=True)
+center_distance_densities, center_distance_bins = np.histogram(center_distance, bins=np.linspace(0, 400, 30), density=True)
 pl.figure()
 pl.title("Center distance densities")
-pl.plot(b[1:], a)
-print("Center distance densities bins: ", b[1:])
-print("Center distance densities: ", a)
+pl.plot(center_distance_bins[1:], center_distance_densities)
+print("Center distance densities bins: ", center_distance_bins[1:])
+print("Center distance densities: ", center_distance_densities)
 print("Median distance to the center: ", np.nanmedian(center_distance))
 print("\n\n")
 
 ##################
-a,b = np.histogram(interbout_intervals, bins=np.linspace(0, 3, 30), density=True)
+interbout_interval_densities,interbout_interval_bins = np.histogram(interbout_intervals, bins=np.linspace(0, 3, 30), density=True)
 pl.figure()
 pl.title("Interbout interval histograms")
-pl.plot(b[1:], a)
-print("Interbout interval bins:", b[1:])
-print("Interbout interval densities: ", a)
+pl.plot(interbout_interval_bins[1:], interbout_interval_densities)
+print("Interbout interval bins:", interbout_interval_bins[1:])
+print("Interbout interval densities: ", interbout_interval_densities)
 print("Median Interbout interval:", np.nanmean(interbout_intervals))
 
 
 pl.show()
+
+dframe = DataFrame({"Swims forward bins: ": swims_forward_bins[1:],
+                    "Swims forward densities: ": swims_forward_densities,
+
+                    "Turn angle bins: ": turn_angle_bins[1:],
+                    "Turn angle densities: ": turn_angle_densties,
+
+                    'Center distance densities bins: ': center_distance_bins[1:],
+                    'Center distance densities: ': center_distance_densities,
+
+                    'Interbout interval bins:': interbout_interval_bins[1:],
+                    'Interbout interval densities:': interbout_interval_densities})
+
+dframe.to_excel('histograms.xlsx', sheet_name='histograms', index=False)
+
+dframe = DataFrame({"Median forward swims: ": [np.nanmedian(np.abs(bout_path_changes))],
+                    "Median absolute turn angle: ": [np.nanmedian(np.abs(bout_angle_changes))],
+                    "Median distance to the center: ": [np.nanmedian(center_distance)],
+                    "Median Interbout interval: ": [np.nanmean(interbout_intervals)],
+                    })
+
+dframe.to_excel('medians.xlsx', sheet_name='medians', index=False)
+
+
+
+# make an excel file
 
